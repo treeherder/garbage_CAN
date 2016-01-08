39244c7ff987e812144974780a0d395c9774cb63
brendan <b.d.reddybest@gmail.com>
2016-01-08 15:10:18 +0000

    working to parse out the :body keyword

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject curler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-:dependencies [[org.clojure/clojure "1.7.0"]
-  [org.clojure/data.json "0.2.6"]
-  [clj-http "2.0.0"]]
+  :description "an offshoot of another project by the same name"
+  :url "http://packetfire.org/"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+  		[org.clojure/data.json "0.2.6"]
+ 		 [clj-http "2.0.0"]]
   :main ^:skip-aot curler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/curler/core.clj
+++ src/curler/core.clj
@@ -8,7 +8,20 @@
   "with given parameters, this function will return a page from the league API call"
   [api_key summoner_name] 
   (println "posting http GET to league API for relevant data...")
-  (print (client/get (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key)) )
+
+;; first take the json from the api
+
+(let [API_DATA (client/get (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key))]
+ ( spit "/tmp/test.json"  (json/write-str API_DATA))
+
+;; slurp the json
+
+( let [keyworded
+  (json/read-str (slurp "/tmp/test.json") :key-fn keyword)]
+  (println (keyworded :body))))
+
+
+;; end main
 
 )
 
