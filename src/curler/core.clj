3fbde22cbe6cf2235170d9664753c6baf104123b
brendan <b.d.reddybest@gmail.com>
2016-01-09 00:57:33 +0000

    get game data from summoner name

--- src/curler/core.clj
+++ src/curler/core.clj
@@ -4,22 +4,27 @@
 
 (:gen-class) )
 
-(defn get_id
+(defn by_summoner_name
   "return ID from summoner name"
   [api_key summoner_name] 
 
   (println "posting http GET to league API for relevant data...")
 
-(let [API_DATA (json/write-str (client/get (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key)))]
-  (let [PARSED ((json/read-str API_DATA :key-fn keyword) :body)]
- 	(let [BODY ((json/read-str PARSED :key-fn keyword) (keyword summoner_name))]
- 	 
- 	(println (BODY :id))	
-))))
+(let [api_data (json/write-str (client/get (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key)))]
+  (let [parsed ((json/read-str api_data :key-fn keyword) :body)]
+ 	(let [body ((json/read-str parsed :key-fn keyword) (keyword summoner_name))]
+ 	(let [sum_id (body :id)](println "got id") sum_id))))
+)
+
+(defn query_game
+	"use summoner ID from name to"
+	[summoner_id api_key]
+	(println "using id")
+	(let [api_data (json/write-str (client/get (format "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/%s?api_key=%s" summoner_id api_key)))]
+		(let [parsed ((json/read-str api_data :key-fn keyword) :body)]
+		parsed)))
 
 (defn -main
   "put everything together, use the same arguments"
   [api_key summoner_name]
-  (get_id api_key summoner_name)
-
- )
+  (println (query_game(by_summoner_name api_key summoner_name) api_key)))
