2f764ad88040e43eb22ca08c7a77683958f75a30
brendan <b.d.reddybest@gmail.com>
2016-01-12 16:37:55 -0800

    generating runme herefile

--- src/curler/core.clj
+++ src/curler/core.clj
@@ -1,8 +1,12 @@
+
 (ns curler.core
+
 (:require [clojure.data.json :as json])
-(require [clj-http.client :as client])
+(:require [clj-http.client :as client])
+(:require [clojure.string :as strings])
 
 (:gen-class) )
+
 
 (defn by_summoner_name
   "return ID from summoner name"
@@ -10,7 +14,8 @@
 
   (println "posting http GET to league API for relevant data...")
 
-(let [api_data (json/write-str (client/get (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key)))]
+(let [api_data (json/write-str (client/get 
+  (format "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/%s?api_key=%s" summoner_name api_key)))]
   (let [parsed ((json/read-str api_data :key-fn keyword) :body)]
  	(let [body ((json/read-str parsed :key-fn keyword) (keyword summoner_name))]
  	(let [sum_id (body :id)](println "got id") sum_id))))
@@ -22,7 +27,8 @@
 	for the observer stream cypher"
 	[summoner_id api_key]
   (try
-      (let [api_data (json/write-str (client/get (format "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/%s?api_key=%s" summoner_id api_key)))]
+      (let [api_data (json/write-str (client/get 
+        (format "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/%s?api_key=%s" summoner_id api_key)))]
         (let [parsed ((json/read-str api_data :key-fn keyword) :body)]
           (let[body (json/read-str parsed :key-fn keyword)]  ;; not sure why this is necessary... seems like json datatype should persist through levels
               (let [[participants, game_id observer_data :as game_data]
@@ -38,17 +44,15 @@
 
 (defn get_chunk
   "download a chunk"
-  [game_id chunk_num]
-(client/get (format"http://spectator.na.lol.riotgames.com/observer-mode/rest/consumer/getGameDataChunk/NA1/%s/%s/token" game_id chunk_num))
-  )
+  [game_id chunk_num] 
+  (client/get (format "http://spectator.na.lol.riotgames.com/observer-mode/rest/consumer/getGameDataChunk/NA1/%s/%s/token" game_id chunk_num)))
 ;;getting chunks isn't going to help us until we can decode the binary file that 
-;;results from the dexrypted / deompressed chunk
+;;results from the decrypted / decompressed chunk
 
 
 (defn observer
   "launch observer client"
-[api_key summoner_name]
-
+  [api_key summoner_name]
   (try 
     (let [game_id (query_game (by_summoner_name api_key summoner_name) api_key)]
       (println game_id))
@@ -61,7 +65,8 @@
   [api_key summoner_name]
   (let [game_data (query_game (by_summoner_name api_key summoner_name) api_key)]
     (let [[participants game_id observers] game_data]
-      ;;(println participants)
-      (println game_id)
-      (println (observers :encryptionKey))
-      )))
+      ;;(println participants)  ;; this gives us a field will all the current game participants ala lolnexus
+      (println )
+      (let [base (slurp "resources/windows")]
+          (spit "dev-resources/runme" (format "%s %s %s NA1\"" base (observers :encryptionKey) game_id))
+          ))))
